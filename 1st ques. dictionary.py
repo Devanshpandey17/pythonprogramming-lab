0e0078639ce4ea46a7e4d28f6bd291e0361ccd62
Devansh Pandey <142798540+Devanshpandey17@users.noreply.github.com>
2023-11-21 13:03:37 +0530

    Add files via upload

---
+++ 1st ques. dictionary.py
@@ -0,0 +1,13 @@
+info1={'name':'jatin',
+       'rolln':34,
+       'college':'GLA UNIVERSITY',
+       'cpi':4.5}
+
+new_info={'name':'Jatin Thakur',
+          'mobile':7865112345}
+
+for i in new_info:
+    info1[i]=new_info[i]
+
+
+print(info1)
